6981da602bf2ea524677728a620b121143f0d5c4
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-04-30 14:17:57 -0300

    Implement random dice roll

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let imageLiterals = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Display random dice roll when the app loads up.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: imageLiterals.randomElement()!)
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: imageLiterals.randomElement()!)
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // Everytime the user presses the Roll button display random dice roll.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: imageLiterals.randomElement()!)
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: imageLiterals.randomElement()!)
+    }
 }
-
